3044c3a69d3f0d3679d7e626ea04e91662ab6d3c
TGaDev203 <hoangthuychimte@gmail.com>
2024-08-30 14:09:51 +0700

    Initialize Project

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+import org.jfugue.player.Player;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    Player player = new Player();
+    player.play("C D E F G A B C6");
+  }
+}
